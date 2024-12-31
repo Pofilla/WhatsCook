97698e396a86fe3fa9fc523ff24c10f933334c39
Reza Ahmadizadeh <mr.ahmadizadeh28@gmail.com>
2024-12-31 17:13:12 +0330

    Initial Commit

---
+++ WhatsCook/WhatsCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsCookApp.swift
+//  WhatsCook
+//
+//  Created by Reza Ahmadizadeh on 10/11/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct WhatsCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
